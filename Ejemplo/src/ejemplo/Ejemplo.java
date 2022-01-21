f21e75a6f5fa31a6e5d758148f1acbd4584c0c9d
Pablo Costa <pacosta2@utpl.edu.ec>
2022-01-21 12:16:04 -0500

    Commit

--- Ejemplo/src/ejemplo/Ejemplo.java
+++ Ejemplo/src/ejemplo/Ejemplo.java
@@ -5,17 +5,32 @@
  */
 package ejemplo;
 
+import org.apache.commons.lang3.RandomUtils;
 /**
  *
  * @author reroes
  */
 public class Ejemplo {
-
+                      // jonathan coronel
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        int par1 = 100;
+        int par2 = 110;
+       
+        Recorrer(par1,par2);
+        
     
+    }
+    public static void Recorrer (int par1, int par2){
+           int[] a = new int[10];
+           for (int i = 0; i < a.length; i++) {
+              a[i] = RandomUtils.nextInt(par1, par2);
+              System.out.println(a[i]);
+        }
+           
+    }
+            
 }
